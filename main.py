7efada0975539276fd0580ec90697806339ba961
Jeonguk Yim <yju0808@naver.com>
2020-03-26 23:46:31 +0900

    예외처리, 음악추천 개편

--- main.py
+++ main.py
@@ -7,43 +7,80 @@ import random
 app = Flask(__name__)
 
 
-blockIdsfile = open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJU bot\\secretData\\blockIds.txt",encoding="UTF-8")
+blockIdsfile = open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJUBOT\\secretdata\\blockIds.txt",encoding="UTF-8")
 blockIds = json.loads(blockIdsfile.read())
 blockIdsfile.close()
 
 
 def Meal(inputYear,inputMonth,inputDay,discrimination):
-    year = inputYear
-    month = inputMonth
-    day = inputDay
 
-    mealDataFile=open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJU bot\\newdatabase.json",encoding="utf-8")
-    mealData = json.loads(mealDataFile.read())["mealServiceDietInfo"][1]["row"]
-    mealDataFile.close()
+    try:
+        year = inputYear
+        month = inputMonth
+        day = inputDay
+
+        mealDataFile=open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJUBOT\\newdatabase.json",encoding="utf-8")
+        mealData = json.loads(mealDataFile.read())["mealServiceDietInfo"][1]["row"]
+
+        breakfast = "해당하는 급식이 없습니다"
+        lunch = "해당하는 급식이 없습니다"
+        dinner = "해당하는 급식이 없습니다"
 
 
-    breakfast = "해당하는 급식이 없습니다"
-    lunch = "해당하는 급식이 없습니다"
-    dinner = "해당하는 급식이 없습니다"
+        for i in mealData:
+            if i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="조식":
+                breakfast = i["DDISH_NM"]
+
+            elif i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="중식":
+                lunch = i["DDISH_NM"]
+
+            elif i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="석식":
+                dinner = i["DDISH_NM"]
+        
+        res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "{}월 {}일 [조식]".format(month,day)+"\n"+breakfast+"\n\n"+ "{}월 {}일 [중식]".format(month,day)+"\n"+lunch+"\n\n"+"{}월 {}일 [석식]".format(month,day)+"\n"+dinner
+                        }
+                    }
+                ],
+                "quickReplies": [
+                    {
+                        "label":"홈으로",
+                        "action":"block",
+                        "blockId" : blockIds["Home"],
+
+                    },
+                ]
+            }
+        }
+
+        if discrimination=="TodayMeal":
+            res["template"]["quickReplies"].append({"label":"내일급식은?", "action":"block", "blockId" : blockIds["TomorrowMeal"],})
+            res["template"]["quickReplies"].append({"label": "날짜선택","action":"block","blockId" : blockIds["SelectDate"],})
+        elif discrimination=="TomorrowMeal":
+            res["template"]["quickReplies"].append({"label":"오늘급식은?", "action":"block", "blockId" : blockIds["TodayMeal"],})
+            res["template"]["quickReplies"].append({"label": "날짜선택","action":"block","blockId" : blockIds["SelectDate"],})
+        elif discrimination=="SelectDate":
+            res["template"]["quickReplies"].append({"label":"오늘급식은?", "action":"block", "blockId" : blockIds["TodayMeal"],})
+            res["template"]["quickReplies"].append({"label":"내일급식은?", "action":"block", "blockId" : blockIds["TomorrowMeal"],})
+            res["template"]["quickReplies"].append({"label": "날짜선택","action":"block","blockId" : blockIds["SelectDate"],})
 
 
-    for i in mealData:
-        if i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="조식":
-            breakfast = i["DDISH_NM"]
+        
 
-        elif i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="중식":
-            lunch = i["DDISH_NM"]
 
-        elif i["MLSV_YMD"]=="{}{}{}".format(year,month,day) and i["MMEAL_SC_NM"]=="석식":
-            dinner = i["DDISH_NM"]
-    
-    res = {
+    except Exception as e:
+        res = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "{}월 {}일 [조식]".format(month,day)+"\n"+breakfast+"\n\n"+ "{}월 {}일 [중식]".format(month,day)+"\n"+lunch+"\n\n"+"{}월 {}일 [석식]".format(month,day)+"\n"+dinner
+                        "text": "버그가 발생했습니다. 개발자님께 버그를 제보할게요"
                     }
                 }
             ],
@@ -54,25 +91,17 @@ def Meal(inputYear,inputMonth,inputDay,discrimination):
                     "blockId" : blockIds["Home"],
 
                 },
-                {
-                    "label": "날짜선택",
-                    "action":"block",
-                    "blockId" : blockIds["SelectDate"],
-                }
             ]
         }
-    }
+        }
 
-    if discrimination=="TodayMeal":
-        res["template"]["quickReplies"].append({"label":"내일급식은?", "action":"block", "blockId" : blockIds["TomorrowMeal"],})
-    elif discrimination=="TomorrowMeal":
-        res["template"]["quickReplies"].append({"label":"오늘급식은?", "action":"block", "blockId" : blockIds["TodayMeal"],})
-    elif discrimination=="SelectDate":
-        res["template"]["quickReplies"].append({"label":"오늘급식은?", "action":"block", "blockId" : blockIds["TodayMeal"],})
-        res["template"]["quickReplies"].append({"label":"내일급식은?", "action":"block", "blockId" : blockIds["TomorrowMeal"],})
+        print("\n\n예외가 발생했습니다 : {}".format(e))
+        
 
+    finally:
+        mealDataFile.close()
+        return res
 
-    return res
 
 
 
@@ -160,58 +189,84 @@ def SelectDate():
 
 @app.route('/music', methods=['POST'])
 def music():
-    musicListFile = open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJU bot\\musiclist.txt",encoding="utf-8")
-    musicList = musicListFile.read().split("\n\n")
-    musicListFile.close()
 
-    musicAndURL = random.choice(musicList).split("|")
-    music = musicAndURL[0]
-    URL = musicAndURL[1]
+    try:
+        musicListFile = open("C:\\Users\\yju08\\OneDrive\\동기화폴더\\서버용폴더\\YJUBOT\\musiclist.txt",encoding="utf-8")
+        recommendedMusic = random.choice(musicListFile.read().split("|")).split("$")
 
-    res = {
-    "version": "2.0",
-    "template": {
-        "outputs": [
-            {
-                "carousel": {
-                "type": "basicCard",
-                "items": [
+        res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
                 {
-                    "title": music,
-                    "description": URL,
-                    "thumbnail": {
-                    "imageUrl": "https://ifh.cc/g/8NMLi.png"
+                    "carousel": {
+                    "type": "basicCard",
+                    "items": [
+                    {
+                        "title": recommendedMusic[0]+"- "+recommendedMusic[1],
+                        "description": recommendedMusic[3],
+                        "thumbnail": {
+                        "imageUrl": recommendedMusic[2]
+                        }
+                    }
+
+                ]
+            }
+        }
+        ],
+        "quickReplies":[
+        {
+        "label":"홈으로",
+        "action":"block",
+        "blockId" : blockIds["Home"],
+
+        },
+        {
+        "label":"음악추천 더해줘",
+        "action":"block",
+        "blockId" : blockIds["MusicRecommendation"],
+
+        },
+        {
+        "label":"음악 더보기",
+        "action":"block",
+        "blockId" : blockIds["MoreMusic"],
+
+        }
+        ]
+        }
+        }
+
+    except Exception as e:
+
+        res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "버그가 발생했습니다. 개발자님께 버그를 제보할게요"
                     }
                 }
+            ],
+            "quickReplies": [
+                {
+                    "label":"홈으로",
+                    "action":"block",
+                    "blockId" : blockIds["Home"],
 
+                },
             ]
         }
-    }
-    ],
-    "quickReplies":[
-    {
-      "label":"홈으로",
-      "action":"block",
-      "blockId" : blockIds["Home"],
+        }
 
-    },
-    {
-      "label":"음악추천 더해줘",
-      "action":"block",
-      "blockId" : blockIds["MusicRecommendation"],
+        print("\n\n예외가 발생했습니다 : {}".format(e))
+        print(recommendedMusic+"\n\n")
+        
 
-    },
-    {
-      "label":"음악 더보기",
-      "action":"block",
-      "blockId" : blockIds["MoreMusic"],
-
-    }
-    ]
-    }
-    }
-
-    return jsonify(res)
+    finally:
+        musicListFile.close()
+        return jsonify(res)
 
 
 
@@ -223,7 +278,5 @@ def music():
 
 
 
-
-# 메인 함수
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
